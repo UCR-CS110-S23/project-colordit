0a7bc32fde357e73ec8a90ab765b243bcfa85840
okay-son <soyeonsonialee@gmail.com>
2023-06-04 19:43:08 -0700

    finished register component

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.svg"
 import { ToastContainer,toast } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import axios from "axios"
 import { registerRoute } from '../utils/APIRoutes';
 
+
 function Register() {
+
+    const navigate = useNavigate;
 
     const [values,setValues] = useState({
         username: "",
@@ -28,12 +31,20 @@ function Register() {
         event.preventDefault();
         // alert("form");
         if (handleValidation()) {
-            const { password, confirmPassword, username, email } = values;
-                const {data} = await axios.post(registerRoute, {
+            console.log("in validation", registerRoute);
+            const { username, email, password } = values;
+                const { data } = await axios.post(registerRoute, {
                     username,
                     email,
                     password,
                 });
+            if (data.status === false) {
+                toast.error(data.msg, toastOptions);
+            }
+            if (data.status === true) {
+                localStorage.setItem('chat-app-user', JSON.stringify(data.user))
+                navigate("/");
+            }
         }
     };
 
@@ -64,6 +75,8 @@ function Register() {
                 toastOptions
             )
         }
+
+        return true;
     }
 
     const handleChange = (event) => {
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 require("dotenv").config();
@@ -8,7 +9,9 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL,{
+app.use("/api/auth", userRoutes)
+
+mongoose.connect('mongodb+srv://slee827:OMp4Dnb0MBQVUcBz@colordit.wuf0kzx.mongodb.net/',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
