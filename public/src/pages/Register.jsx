4f4cf8f7c3497b44a4b5a3b26b42c5b32624698d
okay-son <soyeonsonialee@gmail.com>
2023-06-04 17:30:27 -0700

    register page almost set up

---
+++ public/src/pages/Register.jsx
@@ -0,0 +1,185 @@
+import React, { useState, useEffect } from 'react';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.svg"
+import { ToastContainer,toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import axios from "axios"
+import { registerRoute } from '../utils/APIRoutes';
+
+function Register() {
+
+    const [values,setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const toastOptions = {
+        position: 'bottom-right',
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        // alert("form");
+        if (handleValidation()) {
+            const { password, confirmPassword, username, email } = values;
+                const {data} = await axios.post(registerRoute, {
+                    username,
+                    email,
+                    password,
+                });
+        }
+    };
+
+    const handleValidation =() => {
+        const { password, confirmPassword, username, email } = values;
+        if (password !== confirmPassword) {
+            toast.error(
+                "Password and confirm password do not match.", 
+                toastOptions
+            );
+            return false;
+        }
+        else if (username.length < 3) {
+            toast.error(
+                "Username should be at least 3 characters.",
+                toastOptions
+            );
+        }
+        else if (password.length < 8) {
+            toast.error(
+                "Password should be at least 8 characters.",
+                toastOptions
+            );
+        }
+        else if (email==="") {
+            toast.error(
+                "Email is required.",
+                toastOptions
+            )
+        }
+    }
+
+    const handleChange = (event) => {
+        setValues({ ...values,[event.target.name]:event.target.value })
+    };
+
+    return (
+        <>
+            <FormContainer>
+                <form onSubmit={(event)=>handleSubmit(event)}>
+                    <div className="brand">
+                        <img src={Logo} alt='logo' />
+                        <h1>
+                            colordit chat
+                        </h1>
+                    </div>
+                    <input 
+                        type='text' 
+                        placeholder='Username' 
+                        name='username' 
+                        onChange={(e) => handleChange(e)} 
+                    />
+                    <input 
+                        type='email' 
+                        placeholder='Email' 
+                        name='email' 
+                        onChange={(e) => handleChange(e)} 
+                    />
+                    <input 
+                        type='password' 
+                        placeholder='Password' 
+                        name='password' 
+                        onChange={(e) => handleChange(e)} 
+                    />
+                    <input 
+                        type='password' 
+                        placeholder='Confirm Password' 
+                        name='confirmPassword' 
+                        onChange={(e) => handleChange(e)} 
+                    />
+                    <button type='submit'>Create User</button>
+                    <span>already have an account? <Link to="/login">Login</Link></span>
+                </form>
+            </FormContainer>
+            <ToastContainer />
+        </>
+    )
+}
+
+const FormContainer = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+    align-items: center;
+    background-color: #26184a;
+    .brand {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        justify-content: center;
+        img {
+            height: 5rem;
+        }
+        h1 {
+            color: white;
+            text-transform: uppercase;
+        }
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        background-color: #1d1238;
+        border-radius: 2rem;
+        padding: 3rem 5rem;
+        input {
+            background-color: transparent;
+            padding: 1rem;
+            border: 0.1rem solid #4e0eff;
+            border-radius: 0.4rem;
+            color: white;
+            width: 100%;
+            font-size: 1rem;
+            &:focus {
+                border: 0.1rem solid #997af0;
+                outline: none;
+            }
+        }
+        button {
+            background-color: #997af0;
+            color: white;
+            padding: 1rem 2rem;
+            border: none;
+            font-weight: bold;
+            cursor: pointer;
+            border-radius: 0.4rem;
+            font-size: 1rem;
+            text-transform: uppercase;
+            transition: 0.3s ease-in-out;
+            &:hover {
+                background-color: #5636a7;
+            }
+        }
+        span {
+            color: white;
+            text-transform: uppercase;
+            a {
+                color: #4e0eff;
+                text-decoration: none;
+                font-weight: bold;
+            }
+        }
+    }
+`;
+
+export default Register
---
+++ public/src/utils/APIRoutes.js
@@ -0,0 +1,2 @@
+const host = "http://localhost:3000";
+export const registerRoute = `${host}/api/auth/login`;
