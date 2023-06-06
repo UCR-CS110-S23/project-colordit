16647274b072fd912f035caf0b90c68d464c25bc
okay-son <soyeonsonialee@gmail.com>
2023-06-06 00:17:56 -0700

    contacts update again

--- public/src/components/Contacts.jsx
+++ public/src/components/Contacts.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
 
-export default function Contacts({contacts, currentUser}) {
+export default function Contacts({contacts, currentUser, changeChat}) {
   const [currentUserName, setCurrentUserName] = useState(undefined);
   const [currentUserImage, setCurrentUserImage] = useState(undefined);
   const [currentSelected, setCurrentSelected] = useState(undefined);
@@ -13,7 +13,10 @@ export default function Contacts({contacts, currentUser}) {
       setCurrentUserName(currentUser.username);
     }
   },[currentUser])
-  const changeCurrentChat = (index, contact) => {}
+  const changeCurrentChat = (index, contact) => {
+    setCurrentSelected(index);
+    changeChat(contact);
+  }
   return <>
   {
     currentUserImage && currentUserName && (
@@ -45,9 +48,9 @@ export default function Contacts({contacts, currentUser}) {
             <img src={`data:image/svg+xml;base64,${currentUserImage}`} alt="avatar" />
           </div>
           <div className="username">
-            <h3>
+            <h2>
               {currentUserName}
-            </h3>
+            </h2>
           </div>
         </div>
       </Container>
@@ -59,7 +62,7 @@ export default function Contacts({contacts, currentUser}) {
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: 10% 75% 15%;
+  grid-template-rows: 10% 75% 15%;
   overflow: hidden;
   background-color: #080420;
   .brand {
@@ -80,9 +83,65 @@ const Container = styled.div`
     flex-direction: column;
     align-items: center;
     overflow: auto;
-    gap: 0.8rem;
+    gap: 0.8rem,
+    &::-webkit-scrollbar {
+      width: 0.2rem;
+      &-thumb {
+        background-color: #ffffff39;
+        width: 0.1rem;
+        border-radius: 1rem;
+      }
+    };
     .contact {
       background-color: #ffffff39;
+      min-height: 5rem;
+      width: 90%;
+      cursor: pointer;
+      border-radius: 0.2rem;
+      padding: 0.4rem;
+      gap: 1rem;
+      align-items: center;
+      display: flex;
+      transitionL 0.5s ease-in-out;
+      .avatar {
+        img {
+          height: 3rem;
+        }
+      }
+      .username {
+        h3 {
+          color: white;
+        }
+      }
+    }
+    .selected {
+      background-color: #9186f3
+    }
+  }
+  .current-user {
+    background-color: #0d0d30;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    .avatar {
+      img {
+        height: 4rem;
+        max-inline-size: 100%;
+      }
+    }
+    .username {
+      h2 {
+        color: white;
+      }
+    }
+    @media screen and (min-width:720px) and (max-width:1080px) {
+      gap: 0.5rem;
+      .username {
+        h2 {
+          font-size: 1rem;
+        }
+      }
     }
   }
 `;
--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -9,6 +9,7 @@ function Chat() {
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
 
   // useEffect(async () => {
   //   if (!localStorage.getItem("chat-app-user")) {
@@ -30,10 +31,14 @@ function Chat() {
   //   }
   // },[currentUser]);
 
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  }
+
   return (
     <Container>
       <div className="container">
-        <Contacts contacts={contacts} currentUser={currentUser}/>
+        <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange}/>
       </div>
     </Container>
   )
