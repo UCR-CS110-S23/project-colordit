0e49be75d03dafc6f69bf4a5bf0fe8be4046118c
Steven Hoang <56275718+xcaracal@users.noreply.github.com>
2023-06-07 10:46:29 -0700

    added selecting an avatar and button

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -61,29 +61,67 @@ function SetAvatar() {
                     )
                 })
             }</div>
+            <button onClick={setProfilePicture} className="submit-btn">
+                 Set as Profile Picture
+            </button>
         </Container>
         <ToastContainer/>
         </>
     );
 }
 
-const Container =  styled.div`
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 3rem;
+  background-color: #131324;
   height: 100vh;
   width: 100vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 1rem;
-  align-items: center;
-  background-color: #26184a;
-  .container {
-    height: 85vh;
-    width: 85vw;
-    background-color: #1d1238;
-    display: grid;
-    grid-template-columns: 25% 75%;
-    @media screen and (min-width:720px) and (max-width:1080px) {
-      grid-template-columns: 35% 65%;
+
+  .loader {
+    max-inline-size: 100%;
+  }
+
+  .title-container {
+    h1 {
+      color: white;
+    }
+  }
+  .avatars {
+    display: flex;
+    gap: 2rem;
+
+    .avatar {
+      border: 0.4rem solid transparent;
+      padding: 0.4rem;
+      border-radius: 5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      img {
+        height: 6rem;
+        transition: 0.5s ease-in-out;
+      }
+    }
+    .selected {
+      border: 0.4rem solid #4e0eff;
+    }
+  }
+  .submit-btn {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
     }
   }
 `;
