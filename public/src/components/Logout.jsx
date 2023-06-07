e79652e21cc2bbd90857d8426acf551317258988
okay-son <soyeonsonialee@gmail.com>
2023-06-06 20:42:40 -0700

    chat frame almost done

---
+++ public/src/components/Logout.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { userNavigate } from "react-router-dom"
+import styled from "styled-components"
+import {BiPowerOff} from "react-icons/bi"
+
+export default function Logout() {
+
+    const navigate = userNavigate();
+    const handleClick = async () => {
+        localStorage.clear();
+        navigate("/login")
+    }
+
+  return (
+    <div>
+        <Button onClick={handleClick}>
+            <BiPowerOff />
+        </Button>
+    </div>
+  )
+}
+
+const Button = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    background-color: #9a86f3;
+    border: none;
+    cursor: pointer;
+    svg {
+        font-size: 1.3rem;
+        color: #ebe7ff;
+    }
+`;
