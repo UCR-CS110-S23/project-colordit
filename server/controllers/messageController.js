1e31ea02ab48324c5f94c6e356800b077d1d1220
Steven Hoang <56275718+xcaracal@users.noreply.github.com>
2023-06-07 17:01:37 -0700

    messages code

---
+++ server/controllers/messageController.js
@@ -0,0 +1,56 @@
+// Importing the Message model
+const Messages = require("../models/messageModel");
+
+// Handling the getMessages request
+module.exports.getMessages = async (req, res, next) => {
+  try {
+    // Extracting 'from' and 'to' from the request body
+    const { from, to } = req.body;
+
+    // Finding messages that match the users
+    const messages = await Messages.find({
+      users: {
+        $all: [from, to],
+      },
+    }).sort({ updatedAt: 1 });
+
+    // Projecting the required fields for the response
+    const projectedMessages = messages.map((msg) => {
+      return {
+        fromSelf: msg.sender.toString() === from,
+        message: msg.message.text,
+      };
+    });
+
+    // Sending the projected messages as a JSON response
+    res.json(projectedMessages);
+  } catch (ex) {
+    // Passing the error to the error handling middleware
+    next(ex);
+  }
+};
+
+// Handling the addMessage request
+module.exports.addMessage = async (req, res, next) => {
+  try {
+    // Extracting 'from', 'to', and 'message' from the request body
+    const { from, to, message } = req.body;
+
+    // Creating a new message document
+    const data = await Messages.create({
+      message: { text: message },
+      users: [from, to],
+      sender: from,
+    });
+
+    // Checking if the message was added successfully
+    if (data) {
+      return res.json({ msg: "Message added successfully." });
+    } else {
+      return res.json({ msg: "Failed to add message to the database" });
+    }
+  } catch (ex) {
+    // Passing the error to the error handling middleware
+    next(ex);
+  }
+};
