8b24ea99b728d01d0c2f143703f894acb88e583a
okay-son <soyeonsonialee@gmail.com>
2023-06-08 15:19:44 -0700

    fix

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,38 +1,6 @@
 // Importing the Message model
 const Messages = require("../models/messageModel");
 
-<<<<<<< HEAD
-// Handling the getMessages request
-module.exports.getAllMessages = async (req, res, next) => {
-  try {
-    // Extracting 'from' and 'to' from the request body
-    const { from, to } = req.body;
-
-    // Finding messages that match the users
-    const messages = await Messages.find({
-      users: {
-        $all: [from, to],
-      },
-    }).sort({ updatedAt: 1 });
-
-    // Projecting the required fields for the response
-    const projectedMessages = messages.map((msg) => {
-      return {
-        fromSelf: msg.sender.toString() === from,
-        message: msg.message.text,
-      };
-    });
-
-    // Sending the projected messages as a JSON response
-    res.json(projectedMessages);
-  } catch (ex) {
-    // Passing the error to the error handling middleware
-    next(ex);
-  }
-};
-
-=======
->>>>>>> a58863afb8bc399bbca32465d35b9cb2a186b775
 // Handling the addMessage request
 module.exports.addMessage = async (req, res, next) => {
   try {
