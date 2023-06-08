02545d48ab260739c1fb13d32bf51d0f2847336f
okay-son <soyeonsonialee@gmail.com>
2023-06-08 15:23:39 -0700

    message component not needed

--- public/src/components/ChatContainer.jsx
+++ public/src/components/ChatContainer.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect, useRef }from 'react'
 import styled from "styled-components"
 import ChatInput from './ChatInput';
-import Messages from "./Messages"
 import axios from 'axios';
 import Logout from './Logout';
 import { getAllMessagesRoute, recieveMessageRoute, sendMessageRoute } from '../utils/APIRoutes';
