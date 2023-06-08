b6cdfa25ef9879dce3677c1b8e2cf2ec190d236c
Steven Hoang <hoangste22@gmail.com>
2023-06-08 14:55:48 -0700

    fixed committed

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -51,12 +51,16 @@ function Chat() {
     <Container>
       <div className="container">
         <ChatRooms currentUser={currentUser} changeChat={handleChatChange}/>
-        {isLoaded && currentChat === undefined ? (
+        <ChatContainer currentChat={currentChat}
+        currentUser={currentUser} />
+        {
+        /*isLoaded && currentChat === undefined ? (
           <Welcome currentUser={currentUser}/>
           ) : (
              <ChatContainer currentChat={currentChat} currentUser = {currentUser}/> 
             )
-        }
+          */
+          }
       </div>
     </Container>
   )
