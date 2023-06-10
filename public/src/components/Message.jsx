feda624581514ca923468119a494c4ff1a87b342
okay-son <soyeonsonialee@gmail.com>
2023-06-09 18:47:04 -0700

    message update

--- public/src/components/Message.jsx
+++ public/src/components/Message.jsx
@@ -5,13 +5,22 @@ export default function Message(m) {
 
     const [like, setLike] = useState(false);
 
-    const reactToMessage = () => {
+    function reactToMessage() {
+        var x = document.getElementById("likeEmoji");
         setLike(!like);
-    };
+        if (like) {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
 
     return (
         <Container>
             <p onDoubleClick={reactToMessage()}>{m}</p>
+            <div id='likeEmoji'>
+                ♡
+            </div>
         </Container>
     );
 
